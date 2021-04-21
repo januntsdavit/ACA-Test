81e6ad1705bb5edf670de958f27dbd130f8894bd
Davit Janunts <januntsdavit@gmail.com>
2021-04-21 13:22:58 +0400

    Added circle.java

--- Circle.java
+++ Circle.java
@@ -5,18 +5,29 @@ public class Circle {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+
         // Prompt the user to enter a point
         System.out.print("Enter a point with two coordinates: ");
         double x = input.nextDouble();
         double y = input.nextDouble();
-        double z=Math.pow(Math.pow(x, 2) + Math.pow(y, 2), 0.5);
-        String point=(z<4)? "inside" : (z>4)? "outside of": (z==4) ? "on": " ";
+        double z=Math.pow(Math.pow(x-3, 2) + Math.pow(y-5, 2), 0.5);
 
-        // Check whether the point is within the circle
-       
-        System.out.println("Point B with x and y coordinates is  " + point + "the circle line");
-          
-    
+        switch ((z<4)? 0: ((z>4)? 1:  (z==4)? 2: 3)) {
+
+        case 0:
+        System.out.println("Point B with x and y coordinates is inside of the circle line");
+        break;
+        case 1:
+        System.out.println("Point B with x and y coordinates is outside of the circle line");
+        break;
+        case 2:
+        System.out.println("Point B with x and y coordinates is on the circle line");
+        break;
+        case 3:
+        System.out.println("Not a number");
+
+        }
+
         }
 
 
--- Range.java
+++ Range.java
@@ -1,29 +1,25 @@
-  import java.util.Scanner;
+import java.util.Scanner;
 
 public class Range {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        double num = input.nextDouble();
 
-    Switch ((0 <= input && input <= 45 ) ? 0 :
-    (45 <= input && input <= 90) ? 1 : (90 <= input && input <= 135)? 2:" ") {
-             case 0:
-                System.out.println("Number is in...");
+        switch ((0 <= num && num <= 45 ) ? 0 :
+                (45 < num && num <= 90) ? 1 : 
+                (90 < num && num <= 135 )? 2: 3) {
+
+            case 0:
+                System.out.println("number is in 0->45 range");
                 break;
             case 1:
-                System.out.println("I'm between 6 and 10 inclusive.");
+                System.out.println("number is in 45->90 range");
                 break;
             case 2:
-                System.out.println("I'm not between one and five or 6 and 10 inclusive.");
+                System.out.println("number is in 90->135 range");
                 break;
-
-
-
-    }
-
-    
-
-
-
+            case 3: System.out.println("number is in other range");
+        }
     }
 
 
