b39c6c438d4ff613fb13472cdd892944bae9b1d1
Davit Janunts <januntsdavit@gmail.com>
2021-04-21 12:48:32 +0400

    Added Range.java

--- Range.java
+++ Range.java
@@ -1,3 +1,30 @@
+  import java.util.Scanner;
+
 public class Range {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+    Switch ((0 <= input && input <= 45 ) ? 0 :
+    (45 <= input && input <= 90) ? 1 : (90 <= input && input <= 135)? 2:" ") {
+             case 0:
+                System.out.println("Number is in...");
+                break;
+            case 1:
+                System.out.println("I'm between 6 and 10 inclusive.");
+                break;
+            case 2:
+                System.out.println("I'm not between one and five or 6 and 10 inclusive.");
+                break;
+
+
+
+    }
+
     
+
+
+
+    }
+
+
 }
